0ccd94f4dc46a1ac63408d1236751efedac10f1e
Illia Siutkin <illiasiutkin07@gmail.com>
2025-06-09 16:31:21 -0500

    Refactor and add README

--- index.js
+++ index.js
@@ -1,92 +1,129 @@
-let apiKey;
+const MAP_CONFIG = {
+    INITIAL_ZOOM: 13,
+    MAX_ZOOM: 19,
+    TILE_LAYER_URL: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    ATTRIBUTION: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+};
 
-// Get api key from JSON file
-async function initializeApp() {
-    // const response = await fetch('config.json');
-    // const config = await response.json();
-    // apiKey = config.apiKey;
+class MapController {
+    constructor() {
+        this.map = null;
+        this.routingControl = null;
+        this.userLocation = {
+            latitude: null,
+            longitude: null
+        };
+    }
 
-    navigator.geolocation.getCurrentPosition(displayMap, error => {
-        alert("Could not obtain geolocation: " + error.message);
-    }, {
-        enableHighAccuracy: true,
-    });
-}
+    async initialize() {
+        try {
+            await this.getUserLocation();
+            this.initializeMap();
+            this.setupEventListeners();
+        } catch (error) {
+            alert(`Could not obtain geolocation: ${error.message}`);
+        }
+    }
 
-let userLatitude, userLongitude;
-let control;
+    async getUserLocation() {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    this.userLocation = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    };
+                    resolve();
+                },
+                reject,
+                {enableHighAccuracy: true}
+            );
+        });
+    }
 
-function displayMap(position) {
-    userLatitude = position.coords.latitude;
-    userLongitude = position.coords.longitude;
+    initializeMap() {
+        this.map = L.map("map").setView(
+            [this.userLocation.latitude, this.userLocation.longitude],
+            MAP_CONFIG.INITIAL_ZOOM
+        );
 
-    console.log(userLatitude);
-    console.log(userLongitude);
+        L.tileLayer(MAP_CONFIG.TILE_LAYER_URL, {
+            maxZoom: MAP_CONFIG.MAX_ZOOM,
+            attribution: MAP_CONFIG.ATTRIBUTION
+        }).addTo(this.map);
 
-    let map = L.map("map").setView([userLatitude, userLongitude], 13);
+        this.routingControl = L.Routing.control({
+            waypoints: [L.latLng(this.userLocation.latitude, this.userLocation.longitude)],
+            routeWhileDragging: true,
+            waypointMode: "snap",
+            showAlternatives: true
+        }).addTo(this.map);
+    }
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution:
-            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+    setupEventListeners() {
+        this.routingControl.on("routesfound", this.handleRouteFound.bind(this));
+        this.map.on("contextmenu", this.handleContextMenu.bind(this));
+        this.map.on("click", this.handleMapClick.bind(this));
+    }
 
-    control = L.Routing.control({
-        waypoints: [L.latLng(userLatitude, userLongitude)],
-        routeWhileDragging: true,
-        waypointMode: "snap",
-        showAlternatives: true,
-        // router: L.Routing.graphHopper(apiKey, {
-        //     urlParameters: {
-        //         vehicle: 'foot'
-        //     }
-        // }),
+    handleRouteFound(event) {
+        const meterDistance = event.routes[0].summary.totalDistance;
+        const distance = this.calculateDistance(meterDistance);
+        console.log(`${distance.miles.toFixed(2)} mi (${distance.kilometers.toFixed(2)} km)`);
+    }
 
-    }).addTo(map);
+    calculateDistance(meters) {
+        const kilometers = meters / 1000;
+        return {
+            kilometers,
+            miles: kilometers * 0.621371
+        };
+    }
 
-    control.on("routesfound", function (e) {
-        const meterDistance = (e.routes)[0].summary.totalDistance;
-        const kilometerDistance = meterDistance / 1000;
-        const milesDistance = kilometerDistance * 0.621371;
-        console.log(milesDistance.toFixed(2) + " mi (" + kilometerDistance.toFixed(2) + " km)");
-    });
+    handleContextMenu(event) {
+        const container = L.DomUtil.create("div");
+        const startBtn = this.createButton("Start from this location", container);
+        const destBtn = this.createButton("Go to this location", container);
 
-    map.on("contextmenu", function (e) {
-        let container = L.DomUtil.create("div"),
-            startBtn = createButton("Start from this location", container),
-            destBtn = createButton("Go to this location", container);
+        L.popup().setContent(container).setLatLng(event.latlng).openOn(this.map);
 
-        L.popup().setContent(container).setLatLng(e.latlng).openOn(map);
-
-        L.DomEvent.on(startBtn, "click", function () {
-            control.spliceWaypoints(0, 1, e.latlng);
-            map.closePopup();
+        L.DomEvent.on(startBtn, "click", () => {
+            this.routingControl.spliceWaypoints(0, 1, event.latlng);
+            this.map.closePopup();
         });
 
-        L.DomEvent.on(destBtn, "click", function () {
-            control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
-            map.closePopup();
+        L.DomEvent.on(destBtn, "click", () => {
+            this.routingControl.spliceWaypoints(
+                this.routingControl.getWaypoints().length - 1,
+                1,
+                event.latlng
+            );
+            this.map.closePopup();
         });
-    });
+    }
 
-    map.on("click", function (e) {
-        // For some reason, the spliceWaypoints function only updates the route when an element is replaced,
-        // so I'm creating a waypoint and then immediately replacing it
-        control.spliceWaypoints(control.getWaypoints().length - 1, 0, e.latlng);
-        control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
-    });
+    handleMapClick(event) {
+        const waypointsLength = this.routingControl.getWaypoints().length - 1;
+        this.routingControl.spliceWaypoints(waypointsLength, 0, event.latlng);
+        this.routingControl.spliceWaypoints(waypointsLength + 1, 1, event.latlng);
+    }
+
+    createButton(label, container) {
+        const btn = L.DomUtil.create("button", "", container);
+        btn.setAttribute("type", "button");
+        btn.innerHTML = label;
+        return btn;
+    }
+
+    removeLastWaypoint() {
+        this.routingControl.spliceWaypoints(
+            this.routingControl.getWaypoints().length - 1,
+            1
+        );
+    }
 }
 
-function undo() {
-    control.spliceWaypoints(control.getWaypoints().length - 1, 1);
-}
+window.removeLastWaypoint = () => mapController.removeLastWaypoint();
 
-function createButton(label, container) {
-    let btn = L.DomUtil.create("button", "", container);
-    btn.setAttribute("type", "button");
-    btn.innerHTML = label;
-    return btn;
-}
-
-
-initializeApp();
+const mapController = new MapController();
+mapController.initialize();
